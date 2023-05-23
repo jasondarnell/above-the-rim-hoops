c07e3ec3fca79f898cf03c9fc4214c6e1455b203
Jason Darnell <jasdarnell@gmail.com>
2023-05-23 15:00:17 -0500

    update group sessions page

--- src/views/CampsView.vue
+++ src/views/CampsView.vue
@@ -7,21 +7,12 @@
     </v-row>
     <v-row class="justify-center my-2">
       <h3>
-        Train better
+        Group session starting June 5th 
       </h3>
     </v-row>
-    <v-row class="text-left justify-center">
-      <v-col cols="10">
-        
-        Summer group sessions are the perfect opportunity for players to receive a little extra training with peers at a cheaper cost. 
-        Summer group session one details are as follows. 
-        Four week program
-        Girls and boys welcome!
-        Monday nights beginning June 5 and running through June 26
-        Located at Liberty High School Gym
-        Grades 3-5 takes place from 5-6 PM
-        Grades 6-8 takes place from 6-7 PM
-        Cost: $75 for all four sessions total
+    <v-row class="text-center justify-center">
+      <v-col cols="10">        
+        The perfect opportunity for players to receive a little extra training with peers at a cheaper cost. 
       </v-col>
     </v-row>
     <v-row class="justify-center">
@@ -31,31 +22,47 @@
           style="width:100%"
         />
         <br />
-        Sign up is open now!
       </v-col>
+      <v-col cols="6" class="text-left text-body-1">
+        <div class="mx-6">
+          <b>Dates:</b> Mondays June 5th - June 26th (4 weeks) <br />
+          <b>Time:</b><br/>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;5-6 PM (Grades 3-5) <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;6-7 PM (Grades 6-8) <br />
+          <b>Location:</b> Liberty High School Gym <br/>
+          <b>Cost:</b> $75 for all four sessions total
+          <br /><br />
+          Girls and boys are welcome!
+        </div>
+      </v-col>
+    </v-row>
+    <v-row class="justify-center my-3">
+      <h3>Sign up is open now!</h3>
     </v-row>
     <v-row class="justify-center">
       <v-col cols="4">
-        <v-card elevation="5">
-          <v-card-title>
+        <v-card elevation="5" class="pa-2">
+          <v-card-title class="text-h5">
             Grades 3-5
           </v-card-title>
-
-          <v-card-text>
-
-            <b>$75 for all four sessions</b>
-          </v-card-text>
-          <v-card-actions class="justify-center">
-            <v-btn variant="outlined">
-              Register Now (Grad)
+          <v-card-actions class="justify-center mt-3">
+            <v-btn variant="outlined" href="https://checkout.square.site/merchant/MLEFAZF7N9ZFJ/checkout/7FCOLDHJMFZRDYUGQZLGRSZF">
+              Register Now
             </v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
       <v-col cols="4">
-        <v-btn color="white">
-          Register Now 
-        </v-btn>
+        <v-card elevation="5" class="pa-2">
+          <v-card-title class="text-h5">
+            Grades 6-8
+          </v-card-title>
+          <v-card-actions class="justify-center mt-3">
+            <v-btn variant="outlined" href="https://checkout.square.site/merchant/MLEFAZF7N9ZFJ/checkout/7FCOLDHJMFZRDYUGQZLGRSZF">
+              Register Now
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
